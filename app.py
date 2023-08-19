40aa584b308669f6e7aa37ac4fc47465c26f3268
brijeshhere <103956966+brijeshhere@users.noreply.github.com>
2023-08-19 22:49:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ st.write("""
 # Max of three number
 """)
 #Get Input
-input = st.empty()
+
 st.header('User Input Parameters')
 
 
-a = input.number_input("First Number")
-b= input.number_input("Second Number")
-c= input.number_input("third Number")
+a = st.number_input("First Number")
+b= st.number_input("Second Number")
+c= st.number_input("third Number")
 
 st.header('Max of three')
 st.write(max(a,b,c))
